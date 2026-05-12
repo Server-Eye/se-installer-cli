6dac803afcf52a88434e3fc298df8834c40bcb56
servereye Helpdesk <sehelpdesk@servereye.de>
2026-05-12 10:52:01 +0200

    Fix passing of argument list when script is called by the old gpo_install.ps1

--- Deploy-ServerEye.ps1
+++ Deploy-ServerEye.ps1
@@ -550,7 +550,7 @@ function Start-SEInstallation {
 	
 	# Execute ServerEyeSetup.exe with the constructed parameter string
 	try {
-		$installerProcess = Start-Process -FilePath $SetupPath -ArgumentList "ARGUMENTS=\`\"$parameterString\`\" /quiet" -Wait -PassThru -ErrorAction Stop
+		$installerProcess = Start-Process -FilePath $SetupPath -ArgumentList $parameterString -Wait -PassThru -ErrorAction Stop
 		Log "ServerEyeSetup.exe finished with exit code $($installerProcess.ExitCode)." -ToScreen -ToFile
 	} catch {
 		Log "ServerEyeSetup.exe failed to start. Please report this to the servereye Helpdesk. Error: `n$($_.Exception.Message)" -ForegroundColor Red -ToScreen -ToFile
